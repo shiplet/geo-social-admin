f6a5a7f5bbcf8fd59f086472ac4c48d26f317b76
Shiplet <mnshiplet@gmail.com>
2015-08-05 14:07:35 -0600

    versioning

--- GeoSocialAdmin.php
+++ GeoSocialAdmin.php
@@ -2,16 +2,18 @@
 
 class Geo_Social_Admin {
 
-  public function __construct()
-  {
-    
-  }
+    public function __construct()
+    {
 
-  public function success()
-  {
-    
-  }
+    }
 
-  public function 
-  
+    public function success()
+    {
+
+    }
+
+    public function adminOptions()
+    {
+
+    }
 }
--- geo-social-admin.php
+++ geo-social-admin.php
@@ -1,7 +1,24 @@
 <?php
 /*
    Plugin Name: Geo Social Admin
-   Plugin URI: 
-   
-   
-*/
+   Plugin URI: http://github.com/shiplet/geo-social-admin
+   Description: Global social admin settings
+   Version: 0.1
+   Author: Michael Shiplet
+   Author URI: http://shiplet.github.io
+   License: The MIT License (MIT)
+ */
+
+/*
+   Copyright: 2015 Michael Shiplet (email: mnshiplet@gmail.com)
+   All Rights Reserved
+ */
+
+// Include the class
+include('GeoSOcialAdmin.php');
+
+// instantiate the plugin
+$geo_social_admin = new Geo_Social_Admin;
+
+// trigger plugin to run when admin_menu does
+add_action('admin_menu', array($geo_social_admin, 'adminOptions'));
